9f580c02fdde7066c3d293d93fd88776251db758
Star <87891461+starowo@users.noreply.github.com>
2022-12-18 16:56:31 -0800

    Update script.js

--- works/history/trpg/script.js
+++ works/history/trpg/script.js
@@ -29,10 +29,11 @@ class Player extends Entity {
 }
 
 class Weapon {
-    constructor(name, range, atk) {
+    constructor(name, range, atk, matk) {
         this.name = name;
         this.range = range;
         this.atk = atk;
+        this.matk = matk;
     }
 }
 
@@ -66,7 +67,14 @@ class GameEvent {
 const events = [new GameEvent("")];
 
 const weapons = {
-
+    at1837 : new Weapon("Allen & Thurber M1837 revolver pepperbox", 2, 34, 15),
+    c1847 : new Weapon("Colt M1847 Walker revolver", 2, 44, 22),
+    c1860 : new Weapon("Colt M1860 Army revolver", 3, 44, 20),
+    w1850 : new Weapon("Wesson and Leavitt M1850 Dragoon revolver", 3, 40, 30),
+    e1853r : new Weapon("Enfield P1853 rifled musket", 4, 58, 25),
+    g1861c : new Weapon("Gallager M1861 carbine", 3, 52, 33),
+    s1861r : new Weapon("Springfield M1861 rifled musket", 5, 58, 30),
+    w1857 : new Weapon("Whitworth P1857 rifle", 6, 60, 40),
 }
 
 const armors = {
